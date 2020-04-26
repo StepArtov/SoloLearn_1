aea391e06f6e24034e3cf6cadbc1725978020c5f
Stepan <uspita@gmail.com>
2020-04-26 22:19:49 +0300

    Добавьте файлы проекта.

---
+++ SoloLearn_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] b = { 10, 15, 88, 465 };
+            int sum = 0;
+            foreach (int x in b)
+            {
+                sum += x;
+            
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+            
+        }
+    }
+}
